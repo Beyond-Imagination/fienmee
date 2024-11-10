394fc553e3c6098c7c697d80ea24596a641040d1
joonamin <joonamin44@gmail.com>
2024-11-11 00:23:56 +0900

    chore: add 'mongoose' connector

---
+++ apps/server/src/models/connector.ts
@@ -0,0 +1,13 @@
+import { logger } from 'express-winston';
+import mongoose from 'mongoose'
+import { DB_URI, DB_NAME } from '@/config'
+
+export async function connect() {
+    await mongoose.connect(DB_URI, { dbName: DB_NAME});
+    logger.info(`successfully connect mongo db. DB_NAME=${DB_NAME}`)
+}
+
+export async function close() {
+    await mongoose.connection.close(false)
+    logger.info(`successfully close mongo db connection. DB_NAME=${DB_NAME}`)
+}
