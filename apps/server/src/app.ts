cc00a1f6eac7928fa69c343f1169af179a24976d
Yoon Jaehyun <yuchem2@gmail.com>
2024-11-14 21:14:47 +0900

    chore: initialize MongoDB connection on app startup and close on shutdown

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,14 +1,16 @@
 import '@/config'
 import Server from '@/server'
 import { logger } from '@/utils/logger'
+import * as db from '@/models/connector'
 ;(async () => {
+    await db.connect()
     const server = new Server()
-
     server.listen()
 
     async function shutdown() {
         logger.info('gracefully shutdown fienmee')
         await server.close()
+        await db.close()
         logger.info('shutdown complete')
         process.exit()
     }
