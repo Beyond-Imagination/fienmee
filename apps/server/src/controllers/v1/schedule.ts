fe41e57de5430febd8dd655adafe5c6a81bae6d1
강민준 <97105309+joonamin@users.noreply.github.com>
2024-11-29 01:17:10 +0900

    feat: get all `schedules` by userId (#22)

--- apps/server/src/controllers/v1/schedule.ts
+++ apps/server/src/controllers/v1/schedule.ts
@@ -6,10 +6,11 @@ const router = asyncify(express.Router())
 
 // TODO: add validator(eventId, authorId)
 router.post('/', async (req: Request, res: Response) => {
-    const { name, eventId, startDate, endDate, address, location, description, images } = req.body
+    const { name, eventId, authorId, startDate, endDate, address, location, description, images } = req.body
     const schedule = await ScheduleModel.create({
         name: name,
         eventId: eventId,
+        authorId: authorId,
         startDate: startDate,
         endDate: endDate,
         address: address,
@@ -20,6 +21,24 @@ router.post('/', async (req: Request, res: Response) => {
     res.status(201).json({
         scheduleId: schedule._id,
     })
+})
+
+// todo: jwt 에서 user의 identifier 추출 후 미들웨어로 포함
+router.get('/', async (req: Request, res: Response) => {
+    const options = {
+        page: req.query.page || 0,
+        limit: req.query.limit || 100,
+    }
+
+    const filterOption = {
+        from: req.query.from || new Date(0),
+        to: req.query.to || new Date(),
+    }
+
+    const userId = req.body.userId
+    const schedules = await ScheduleModel.findByUserId(userId, filterOption, options)
+
+    res.status(200).json(schedules)
 })
 
 export default router
