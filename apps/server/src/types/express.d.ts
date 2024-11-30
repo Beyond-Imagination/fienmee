08e7743c4d65dad87d59281e6d03a9f9e10edc9c
Kuwon Sebastian Na <laggu91@gmail.com>
2024-11-30 23:14:24 +0900

    chore: add custom field to express request (#31)

---
+++ apps/server/src/types/express.d.ts
@@ -0,0 +1,11 @@
+import { User } from '@/models/user'
+
+declare global {
+    namespace Express {
+        interface Request {
+            _routeWhitelists: { body: string[] }
+            _routeBlacklists: { body: string[] }
+            user: User
+        }
+    }
+}
