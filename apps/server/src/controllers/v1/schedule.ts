05a16cca359003f0625a652057e4a721fe7bbce0
강민준 <97105309+joonamin@users.noreply.github.com>
2024-11-30 18:29:28 +0900

    feat: update schedule API (#26)

--- apps/server/src/controllers/v1/schedule.ts
+++ apps/server/src/controllers/v1/schedule.ts
@@ -41,4 +41,23 @@ router.get('/', async (req: Request, res: Response) => {
     res.status(200).json(schedules)
 })
 
+router.put('/:id', async (req: Request, res: Response) => {
+    // todo: 일정의 authorId와 실제 jwt에 포함된 userId가 같은지 검증하는 middleware 추가
+    const { id } = req.params
+    const { name, startDate, endDate, description, images, location, address } = req.body
+    const updated = await ScheduleModel.findOneAndUpdate(
+        { _id: id },
+        {
+            name: name,
+            startDate: startDate,
+            endDate: endDate,
+            description: description,
+            address: address,
+            location: location,
+            images: images,
+        },
+    )
+    res.status(200).json(updated)
+})
+
 export default router
