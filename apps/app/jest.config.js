f51fe9321ea95fa894b1c9a11592a1fba2ef5b27
Kuwon Sebastian Na <laggu91@gmail.com>
2024-12-01 19:14:06 +0900

    test: mock react-native-webview (#33)

--- apps/app/jest.config.js
+++ apps/app/jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
     preset: 'react-native',
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    transformIgnorePatterns: [
+        '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
+    ],
 }
