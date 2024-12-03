dea42140398a5cb537168136f8cd5cce7963e063
joonamin <joonamin44@gmail.com>
2024-12-04 01:00:11 +0900

    chore: 리뷰 반영

--- apps/server/src/middlewares/schedules.ts
+++ apps/server/src/middlewares/schedules.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
 import { ScheduleModel } from '@/models/schedule'
-import { UnAuthorizedSchedule } from '@/types/errors/schedule'
+import { UnauthorizedSchedule } from '@/types/errors/schedule'
 
 export const verifyAuthorMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const authorId = (await ScheduleModel.findOne({ _id: req.params.id }).exec())?.authorId
     if (!authorId) {
-        throw new UnAuthorizedSchedule(new Error('not found target schedule'))
+        throw new UnauthorizedSchedule(new Error('not found target schedule'))
     }
     if (req.user?._id !== authorId) {
-        throw new UnAuthorizedSchedule()
+        throw new UnauthorizedSchedule()
     }
     next()
 }
--- apps/server/src/types/errors/schedule.ts
+++ apps/server/src/types/errors/schedule.ts
@@ -1,9 +1,9 @@
 import { APIError } from '@/types/errors/error'
 
-export class UnAuthorizedSchedule extends APIError {
+export class UnauthorizedSchedule extends APIError {
     constructor(cause: Error | string = null) {
         super(401, 40100, 'unauthorized schedule', cause)
-        Object.setPrototypeOf(this, UnAuthorizedSchedule.prototype)
-        Error.captureStackTrace(this, UnAuthorizedSchedule)
+        Object.setPrototypeOf(this, UnauthorizedSchedule.prototype)
+        Error.captureStackTrace(this, UnauthorizedSchedule)
     }
 }
