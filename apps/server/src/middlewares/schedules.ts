e48724b125aa94e12a91c51036b45eb0b798682b
강민준 <97105309+joonamin@users.noreply.github.com>
2024-12-09 17:32:36 +0900

    feat: create schedule middleware (#35)

--- apps/server/src/middlewares/schedules.ts
+++ apps/server/src/middlewares/schedules.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import { ScheduleModel } from '@/models/schedule'
-import { UnauthorizedSchedule } from '@/types/errors/schedule'
+import { InvalidRequestFormat, UnauthorizedSchedule } from '@/types/errors/schedule'
+import { EventsModel } from '@/models/event'
+import { UserModel } from '@/models/user'
 
 export const verifyAuthorMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const authorId = (await ScheduleModel.findOne({ _id: req.params.id }).exec())?.authorId
@@ -12,3 +14,23 @@ export const verifyAuthorMiddleware = async (req: Request, res: Response, next: 
     }
     next()
 }
+
+const verifyEventId = async (req: Request, res: Response, next: NextFunction) => {
+    const { eventId } = req.body
+    const target = await EventsModel.findOne({ _id: eventId }).exec()
+    if (!target) {
+        throw new InvalidRequestFormat(new Error(`not found event => id: ${eventId}`))
+    }
+    next()
+}
+
+const verifyAuthorId = async (req: Request, res: Response, next: NextFunction) => {
+    const { authorId } = req.body
+    const target = await UserModel.findOne({ _id: authorId }).exec()
+    if (!target) {
+        throw new InvalidRequestFormat(new Error(`not found author => id: ${authorId}`))
+    }
+    next()
+}
+
+export const addScheduleMiddleware = [verifyAuthorId, verifyEventId]
