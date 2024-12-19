7497aed718c12d78d317bafc4d87cd003a3e23f1
Kuwon Sebastian Na <laggu91@gmail.com>
2024-12-19 21:59:25 +0900

    feat: add empty page (#53)

---
+++ apps/web/src/app/(backButton)/events/update/page.tsx
@@ -0,0 +1,3 @@
+export default function EventUpdate() {
+    return <div className="grid items-center justify-items-center min-h-screen">Event Update</div>
+}
---
+++ apps/web/src/app/(navigation)/setting/page.tsx
@@ -0,0 +1,3 @@
+export default function Setting() {
+    return <div className="grid items-center justify-items-center min-h-screen">setting</div>
+}
