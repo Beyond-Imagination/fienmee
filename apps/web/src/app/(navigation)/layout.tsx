6d3d93cb821f3dc124e9d6f9f26da68c743dc5a2
Kuwon Sebastian Na <laggu91@gmail.com>
2024-12-20 18:55:16 +0900

    feat: navigation (#54)

--- apps/web/src/app/(navigation)/layout.tsx
+++ apps/web/src/app/(navigation)/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Navigation } from '@/components/navigation'
 
 export default function NavigationLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             {children}
-            <div>navigation</div>
+            <Navigation />
         </div>
     )
 }
