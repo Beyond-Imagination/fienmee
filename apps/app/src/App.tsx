62183eb35f10fb85b5a51c7d8277ad31491e2f66
laggu91 <laggu91@gmail.com>
2024-12-21 10:56:55 +0900

    refactor: webview page

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -7,13 +7,12 @@
 
 import React from 'react'
 import { useColorScheme } from 'react-native'
-import { WebView } from 'react-native-webview'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import config from 'react-native-config'
-
 import { Colors } from 'react-native/Libraries/NewAppScreen'
-import LoginScreen from './pages/Login.tsx'
+
+import LoginScreen from './pages/Login'
+import WebviewScreen from './pages/Webview'
 
 function App(): React.JSX.Element {
     const isDarkMode = useColorScheme() === 'dark'
@@ -41,9 +40,7 @@ function App(): React.JSX.Element {
                 />
                 <Stack.Screen
                     name="WebView"
-                    component={() => {
-                        return <WebView source={{ uri: config.FE_URL }} containerStyle={{ flex: 0, width: '100%', height: '100%' }} />
-                    }}
+                    component={WebviewScreen}
                     options={{
                         headerShown: false,
                     }}
