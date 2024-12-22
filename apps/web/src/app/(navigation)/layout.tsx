c56b44923315dcd7155cf320db7e5fb19ac5c8f7
Kuwon Sebastian Na <laggu91@gmail.com>
2024-12-22 21:11:59 +0900

    feat: header (#59)

--- apps/web/src/app/(navigation)/layout.tsx
+++ apps/web/src/app/(navigation)/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { LogoHeader } from '@/components/header'
 import { Navigation } from '@/components/navigation'
 
 export default function NavigationLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
+            <LogoHeader />
             {children}
             <Navigation />
         </div>
