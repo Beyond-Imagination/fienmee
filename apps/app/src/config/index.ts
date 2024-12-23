037b29234c23a8593ac5fef91012e469cbbdb46e
Jaehyun Yoon <yuchem2@gmail.com>
2024-12-23 19:58:21 +0900

    fix: resolve conflicts between default config and .env variables, and prevent duplicate RNCConfigModule loading in Windows environment (#61)

--- apps/app/src/config/index.ts
+++ apps/app/src/config/index.ts
@@ -1,6 +1,6 @@
 import config from 'react-native-config'
 
-export const DEBUG = config.DEBUG === 'true'
+export const ENV = config.ENV === 'develop'
 
 export const BE_URL = config.BE_URL
 export const FE_URL = config.FE_URL
