8a7d9bf785df6abf5323e4647fa0fa9e7b030043
Yoon Jaehyun <yuchem2@gmail.com>
2025-01-01 18:33:30 +0900

    fix: disable overScroll effect in Webview for somoother scrolling

--- apps/app/src/pages/Webview.tsx
+++ apps/app/src/pages/Webview.tsx
@@ -4,5 +4,12 @@ import { WebView } from 'react-native-webview'
 import { ENV, FE_URL } from '../config'
 
 export default function WebviewScreen() {
-    return <WebView source={{ uri: FE_URL }} containerStyle={{ flex: 0, width: '100%', height: '100%' }} webviewDebuggingEnabled={ENV} />
+    return (
+        <WebView
+            source={{ uri: FE_URL }}
+            containerStyle={{ flex: 0, width: '100%', height: '100%' }}
+            webviewDebuggingEnabled={ENV}
+            overScrollMode="never"
+        />
+    )
 }
