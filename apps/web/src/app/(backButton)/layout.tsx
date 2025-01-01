5881e8fd41c98aeab15464d175946650db910ef3
Jaehyun Yoon <yuchem2@gmail.com>
2025-01-01 21:12:50 +0900

    refactor: separate main content from header and navigation (#65)

--- apps/web/src/app/(backButton)/layout.tsx
+++ apps/web/src/app/(backButton)/layout.tsx
@@ -3,9 +3,9 @@ import { PageTitleHeader } from '@/components/header'
 
 export default function BackButtonLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full h-full">
+        <div className="grid w-full h-screen">
             <PageTitleHeader />
-            <div className="w-full h-full">{children}</div>
+            <div className="w-full h-full overflow-y-scroll">{children}</div>
         </div>
     )
 }
