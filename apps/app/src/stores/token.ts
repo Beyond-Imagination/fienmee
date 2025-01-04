97764297843eb50ebfdd076f7a8505a5fd361986
Kuwon Sebastian Na <laggu91@gmail.com>
2025-01-04 22:06:38 +0900

    feat: add token store to mobile app (#70)

---
+++ apps/app/src/stores/token.ts
@@ -0,0 +1,43 @@
+import EncryptedStorage from 'react-native-encrypted-storage'
+
+const TOKEN_KEY = 'jwt'
+
+export async function setToken(token: string): Promise<void> {
+    try {
+        await EncryptedStorage.setItem(
+            TOKEN_KEY,
+            JSON.stringify({
+                token: token,
+            }),
+        )
+    } catch (error) {
+        // TODO: token 저장시 에러 발생 케이스. 호출한 곳에서 모바일 에러 페이지로 이동
+        throw error
+    }
+}
+
+export async function getToken(): Promise<string> {
+    let token: string | null = null
+    try {
+        token = await EncryptedStorage.getItem(TOKEN_KEY)
+    } catch (error) {
+        // TODO: token 불러오는 중 에러 발생 케이스. 호출한 곳에서 모바일 에러 페이지로 이동
+        throw error
+    }
+
+    if (!token) {
+        // TODO: token 없음. 호출한 곳에서 로그인 페이지로 이동
+        throw new Error()
+    }
+
+    return token
+}
+
+export async function deleteToken(): Promise<void> {
+    try {
+        await EncryptedStorage.removeItem(TOKEN_KEY)
+    } catch (error) {
+        // TODO: token 삭제 실패. 호출한 곳에서 모바일 에러 페이지로 이동
+        throw error
+    }
+}
