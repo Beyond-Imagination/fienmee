ef82159d7cdbdc69779e48da1be948a85fbef55f
Kang Minjun <97105309+joonamin@users.noreply.github.com>
2025-01-10 01:02:45 +0900

    feat: logout user (#71)

--- apps/server/src/middlewares/auth.ts
+++ apps/server/src/middlewares/auth.ts
@@ -4,11 +4,16 @@ import { JWT_SECRET } from '@/config'
 import { UnauthorizedTokenError } from '@/types/errors/jwt/auth'
 import { UserModel } from '@/models'
 import { UnknownUserError } from '@/types/errors'
+import { isTokenInBlackList } from '@/utils/blacklist'
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers['authorization']?.split(' ')?.[1]
     if (!token) {
         next(new UnauthorizedTokenError('fill the "authorization" header field'))
+    }
+
+    if (isTokenInBlackList(token)) {
+        next(new UnauthorizedTokenError('Token of logged out user'))
     }
 
     try {
