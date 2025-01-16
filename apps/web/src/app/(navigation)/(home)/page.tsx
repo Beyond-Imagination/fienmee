c8a7bb30bafc3d38ac91240b0ab8b654127f6a35
Yoon Jaehyun <yuchem2@gmail.com>
2025-01-16 22:32:57 +0900

    fix: resolve reviews

--- apps/web/src/app/(navigation)/(home)/page.tsx
+++ apps/web/src/app/(navigation)/(home)/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
             </div>
             <div className="grid gap-10 w-full px-1 pb-10">
                 {/*TODO: change event category*/}
-                <PreviewEventLst header={`${today.getDate()}일 일정`} category="기타" />
+                <PreviewEventLst title={`${today.getDate()}일 일정`} category="기타" />
                 <div className="bg-[#F6F5F5] shadow-inner shadow-[#E4E4E4] w-full h-2" />
-                <PreviewEventLst header="관심 카테고리" category="경관" />
+                <PreviewEventLst title="관심 카테고리" category="경관" />
                 <div className="bg-[#F6F5F5] shadow-inner shadow-[#E4E4E4] w-full h-2" />
-                <PreviewEventLst header="인기 행사" category="콘서트" />
+                <PreviewEventLst title="인기 행사" category="콘서트" />
             </div>
         </div>
     )
