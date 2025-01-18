64a3d3b1c98e24581bdc57c5104a5ac86e8e3267
laggu91 <laggu91@gmail.com>
2025-01-18 14:12:07 +0900

    feat: native stack navigator type

---
+++ apps/app/src/types/screen.ts
@@ -0,0 +1,15 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+export type RootStackParamList = {
+    Login: undefined
+    Register: {
+        accessToken: string
+        refreshToken: string
+        provider: string
+    }
+    WebView: undefined
+}
+
+export type LoginScreenProps = NativeStackScreenProps<RootStackParamList, 'Login'>
+export type RegisterScreenProps = NativeStackScreenProps<RootStackParamList, 'Register'>
+export type WebviewScreenProps = NativeStackScreenProps<RootStackParamList, 'WebView'>
