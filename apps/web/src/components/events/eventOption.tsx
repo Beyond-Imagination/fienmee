e3c69b803424e0be34949dfc9faee68e961d4f44
Mina Lee <chsjen492@gmail.com>
2025-01-23 01:53:34 +0900

    feat: add event delete action to button

--- apps/web/src/components/events/eventOption.tsx
+++ apps/web/src/components/events/eventOption.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Dropdown from '@/components/dropdown'
 import { OptionIcon } from '@/components/icon'
+import { useRouter } from 'next/navigation'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteEventById } from '@/api/event'
 
-export default function EventOption() {
+export default function EventOption({ eventId }: { eventId: string }) {
+    const router = useRouter()
+    const queryClient = useQueryClient()
+
+    const deleteMutation = useMutation({
+        mutationFn: deleteEventById,
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({ queryKey: ['events'] })
+            router.push('/events/category')
+        },
+    })
+
+    const handleDelete = () => {
+        deleteMutation.mutate(eventId)
+    }
+
     return (
         <Dropdown Icon={OptionIcon} xTranslate={'-translate-x-20'}>
             {/* TODO: add modify function */}
             <button className="w-24 p-3 border-b border-gray-200">수정하기</button>
-            {/* TODO: add delete function */}
-            <button className="w-24 p-3">삭제하기</button>
+            <button className="w-24 p-3" onClick={handleDelete}>
+                삭제하기
+            </button>
         </Dropdown>
     )
 }
