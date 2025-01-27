86e4d87cc1abf503db733b12604555bad1aa90d6
정은영 <jey0623@naver.com>
2025-01-27 19:39:50 +0900

    feat: Add event venue selector component with map

---
+++ apps/web/src/components/events/eventVenueSelector.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+
+interface EventVenueSelectorProps {
+    initialPosition: { lat: number; lng: number }
+    onPositionChange: (position: { lat: number; lng: number }) => void
+}
+
+const EventVenueSelector: React.FC<EventVenueSelectorProps> = ({ initialPosition, onPositionChange }) => {
+    const [position, setPosition] = useState(initialPosition)
+
+    const handleMapClick = (_: kakao.maps.Map, mouseEvent: kakao.maps.event.MouseEvent) => {
+        const newPosition = {
+            lat: mouseEvent.latLng.getLat(),
+            lng: mouseEvent.latLng.getLng(),
+        }
+        setPosition(newPosition)
+        onPositionChange(newPosition)
+    }
+
+    return (
+        <Map center={position} style={{ width: '100%', height: '300px' }} level={3} onClick={handleMapClick}>
+            <MapMarker position={position} />
+        </Map>
+    )
+}
+
+export default EventVenueSelector
