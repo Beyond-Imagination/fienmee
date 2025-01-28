ffd2c3dfcb0d30389d42c875deb452f6de95e1b4
laggu91 <laggu91@gmail.com>
2025-01-28 17:33:10 +0900

    feat: logout on app

--- apps/app/src/pages/Webview.tsx
+++ apps/app/src/pages/Webview.tsx
@@ -1,14 +1,28 @@
 import React, { useRef } from 'react'
-import { WebView } from 'react-native-webview'
+import { WebView, WebViewMessageEvent } from 'react-native-webview'
+import { useNavigation } from '@react-navigation/native'
+
+import { IJWTData, isLogoutData } from '@fienmee/types'
 
 import { ENV, FE_URL } from '@/config'
-import { getToken } from '@/stores/token'
+import { deleteToken, getToken } from '@/stores/token'
+import { WebviewScreenProps } from '@/types'
 
 export function WebviewScreen() {
+    const navigation = useNavigation<WebviewScreenProps['navigation']>()
     const webViewRef = useRef<WebView>(null)
     const onLoad = async () => {
         const jwt = await getToken()
-        webViewRef.current?.postMessage(JSON.stringify({ type: 'jwt', jwt: jwt }))
+        webViewRef.current?.postMessage(JSON.stringify({ type: 'jwt', jwt: jwt } as IJWTData))
+    }
+
+    const onMessage = async (e: WebViewMessageEvent) => {
+        const data = e.nativeEvent.data
+
+        if (isLogoutData(data)) {
+            await deleteToken()
+            navigation.navigate('Login')
+        }
     }
 
     return (
@@ -16,6 +30,7 @@ export function WebviewScreen() {
             source={{ uri: FE_URL }}
             ref={webViewRef}
             onLoad={onLoad}
+            onMessage={onMessage}
             containerStyle={{ flex: 0, width: '100%', height: '100%' }}
             webviewDebuggingEnabled={ENV}
             overScrollMode="never"
