6190f26868496c03f281af1f91202f0fe5ebf8de
Kuwon Sebastian Na <laggu91@gmail.com>
2025-02-12 21:51:50 +0900

    feat: issue refresh token (#108)

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -5,4 +5,5 @@ config()
 export const { NODE_ENV, HOST, DB_URI, DB_NAME, JWT_SECRET, SEOUL_API_KEY, SEOUL_API_URL } = process.env
 
 export const PORT = Number.parseInt(process.env.PORT) || 5000
-export const JWT_EXPIRES_IN_MS = Number.parseInt(process.env.JWT_EXPIRES_IN_MS) || 1000 * 60 * 30
+export const ACCESS_TOKEN_EXPIRES_IN_MS = Number.parseInt(process.env.ACCESS_TOKEN_EXPIRES_IN_MS) || 1000 * 60 * 30
+export const REFRESH_TOKEN_EXPIRES_IN_MS = Number.parseInt(process.env.REFRESH_TOKEN_EXPIRES_IN_MS) || 1000 * 60 * 60 * 24 * 60
