bbdc8ef6634e29a3ceb99c1b7ff94d2618861e1e
joonamin <joonamin44@gmail.com>
2025-02-15 20:08:18 +0900

    remove unnecessary `<div>`

--- apps/web/src/components/schedules/scheduleCalendar.tsx
+++ apps/web/src/components/schedules/scheduleCalendar.tsx
@@ -10,28 +10,26 @@ export default function ScheduleCalendar({ onChange }: ScheduleCalendarProps) {
         onChange(value as Date, event)
     }
     return (
-        <div>
-            <div className="bg-white shadow-lg rounded-lg p-4">
-                <Calendar
-                    onChange={handleChange}
-                    calendarType="gregory"
-                    formatMonthYear={(locale, date) => {
-                        const year = date.getFullYear()
-                        const month = date.getMonth() + 1
-                        return `${year}년 ${month}월`
-                    }}
-                    formatDay={(locale, date) => {
-                        return date.getDate()
-                    }}
-                    formatShortWeekday={(locale, date) => {
-                        const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
-                        return weekdays[date.getDay()]
-                    }}
-                    prev2Label={null}
-                    next2Label={null}
-                    showNeighboringMonth={false}
-                />
-            </div>
+        <div className="bg-white shadow-lg rounded-lg p-4">
+            <Calendar
+                onChange={handleChange}
+                calendarType="gregory"
+                formatMonthYear={(locale, date) => {
+                    const year = date.getFullYear()
+                    const month = date.getMonth() + 1
+                    return `${year}년 ${month}월`
+                }}
+                formatDay={(locale, date) => {
+                    return date.getDate()
+                }}
+                formatShortWeekday={(locale, date) => {
+                    const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+                    return weekdays[date.getDay()]
+                }}
+                prev2Label={null}
+                next2Label={null}
+                showNeighboringMonth={false}
+            />
         </div>
     )
 }
