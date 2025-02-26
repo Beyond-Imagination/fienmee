42814b3c17c40d7190c2f65b1e3914f8fa011b26
Yoon Jaehyun <yuchem2@gmail.com>
2025-02-26 17:02:41 +0900

    feat: add delete account request from web to server

--- apps/web/src/components/settings/deleteAcccountModal.tsx
+++ apps/web/src/components/settings/deleteAcccountModal.tsx
@@ -1,4 +1,6 @@
 import { FullScreenDialog } from '@/components/modal'
+import { useMutation } from '@tanstack/react-query'
+import { deleteUser } from '@/api/user'
 
 interface Props {
     isOpen: boolean
@@ -6,6 +8,22 @@ interface Props {
 }
 
 export default function DeleteAccountModal({ isOpen, onClose }: Props) {
+    const deleteQuery = useMutation({
+        mutationFn: () => {
+            return deleteUser()
+        },
+        onSuccess: () => {
+            sessionStorage.removeItem('access_token')
+            if (typeof window !== undefined) {
+                window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'logout' }))
+            }
+            onClose()
+        },
+    })
+    const onDelete = () => {
+        deleteQuery.mutate()
+    }
+
     return (
         <FullScreenDialog isOpen={isOpen} hasClose={false} onClose={onClose}>
             <div className="flex flex-col w-full">
@@ -20,7 +38,9 @@ export default function DeleteAccountModal({ isOpen, onClose }: Props) {
             </div>
             <div className="flex flex-col items-center w-fll gap-3 mt-4">
                 {/*TODO: add action deleting account action*/}
-                <button className="rounded-lg h-12 w-3/4 text-white bg-[#FF9575] py-3">회원 탈퇴</button>
+                <button className="rounded-lg h-12 w-3/4 text-white bg-[#FF9575] py-3" onClick={onDelete}>
+                    회원 탈퇴
+                </button>
                 <button className="rounded-lg h-12 w-3/4 border border-[#78767C] py-3" onClick={onClose}>
                     회원 탈퇴 취소
                 </button>
