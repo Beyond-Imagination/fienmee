bf62637fb1552d9e32cff25416ff905cb62e0a75
정은영 <48662662+rlooo@users.noreply.github.com>
2025-03-01 15:15:52 +0900

    feat: add logo (#130)

--- apps/app/src/pages/Login.tsx
+++ apps/app/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, StyleSheet, ScrollView, Image } from 'react-native'
 import React from 'react'
 
 import { AppleOauthLogin, GoogleOauthLogin, KakaoOauthLogin, NaverOauthLogin } from '@/components/login'
@@ -7,27 +7,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        paddingTop: 100,
+        backgroundColor: '#fffce0',
+    },
+    logoContainer: {
+        flex: 2,
+        width: '60%',
+        height: 300,
+        marginVertical: 30,
+        alignItems: 'stretch',
+        alignSelf: 'center',
+    },
+    logoImage: {
+        width: '100%',
+        height: '100%',
+    },
 })
 
 export function LoginScreen() {
     return (
-        <ScrollView
-            contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
-            style={[styles.container, { paddingTop: 100, backgroundColor: '#FFFFFF' }]}
-        >
-            {/*TODO: change logo*/}
-            <View
-                style={{
-                    flex: 2,
-                    width: '60%',
-                    height: 300,
-                    marginVertical: 30,
-                    alignItems: 'stretch',
-                    alignSelf: 'center',
-                    backgroundColor: '#D9D9D9',
-                }}
-            >
-                <Text>Logo</Text>
+        <ScrollView contentContainerStyle={styles.scrollContainer} style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image source={require('./../../assets/logo.png')} style={styles.logoImage} resizeMode="contain" />
             </View>
             <View style={styles.container}>
                 <KakaoOauthLogin />
