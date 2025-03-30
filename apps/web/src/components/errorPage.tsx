9fca5c966afefbffd21808bdaa49ce7afc52e0d0
sezeme <stellaa223@gmail.com>
2025-03-30 20:44:03 +0900

    feat: 에러 처리 페이지 생성
    라우터 땨료 생성해야하는지 몰라서 일단 페이지만 만들어놓긴 했습니다

---
+++ apps/web/src/components/errorPage.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export function ErrorPage() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+            <h1>문제가 발생했습니다.</h1>
+            <p>이용에 불편을 드려 죄송합니다.</p>
+        </div>
+    )
+}
