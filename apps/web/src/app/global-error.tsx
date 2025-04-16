21d19a5c3919ffabbc094c443401068f756447ed
sezeme <stellaa223@gmail.com>
2025-04-17 01:35:45 +0900

    feat: reset 버튼 추가

--- apps/web/src/app/global-error.tsx
+++ apps/web/src/app/global-error.tsx
@@ -1,15 +1,18 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ErrorPage } from '@/components/errorPage'
 
-export default function GlobalError({ error }: { error: Error }) {
-    console.error(error)
+export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+    useEffect(() => {
+        console.error(error)
+    }, [error])
 
     return (
         <html>
             <body>
                 <ErrorPage />
+                <button onClick={() => reset()}>다시 시도</button>
             </body>
         </html>
     )
