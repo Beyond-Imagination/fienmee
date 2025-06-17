fe9dc6c8da761ada046284d83d7254fa0f1a588e
Mina Lee <chsjen492@gmail.com>
2025-06-17 23:33:46 +0900

    refactor: 리뷰 반영

--- apps/server/src/middlewares/events.ts
+++ apps/server/src/middlewares/events.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 import { CommentsModel } from '@/models'
-import { UnauthorizedComment } from '@/types/errors/events'
+import { CommentNotFound, UnauthorizedComment } from '@/types/errors/events'
 
 export const verifyCommentAuthor = async (req: Request, res: Response, next: NextFunction) => {
-    const authorId = (await CommentsModel.findOne({ _id: req.params.commentId }).exec())?.userId
+    const comment = await CommentsModel.findOne({ _id: req.params.commentId })
+    if (!comment) throw new CommentNotFound()
+    const authorId = comment.userId
     if (!req.user?._id.equals(authorId)) {
         throw new UnauthorizedComment()
     }
--- apps/server/src/types/errors/events.ts
+++ apps/server/src/types/errors/events.ts
@@ -2,8 +2,16 @@ import { APIError } from '@/types/errors/error'
 
 export class UnauthorizedComment extends APIError {
     constructor(cause: Error | string = null) {
-        super(401, 6000, 'unauthorized schedule', cause)
+        super(401, 6000, 'unauthorized comment', cause)
         Object.setPrototypeOf(this, UnauthorizedComment.prototype)
         Error.captureStackTrace(this, UnauthorizedComment)
+    }
+}
+
+export class CommentNotFound extends APIError {
+    constructor(cause: Error | string = null) {
+        super(404, 6001, 'Invalid comment id', cause)
+        Object.setPrototypeOf(this, CommentNotFound.prototype)
+        Error.captureStackTrace(this, CommentNotFound)
     }
 }
