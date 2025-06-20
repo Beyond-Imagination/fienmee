aea25efa989e025b9e07652cee268141d55e98bc
Yoon Jaehyun <yuchem2@gmail.com>
2025-06-20 19:36:59 +0900

    fix typo

--- apps/server/src/types/errors/database.ts
+++ apps/server/src/types/errors/database.ts
@@ -2,7 +2,7 @@ import { APIError } from '@/types/errors/error'
 
 export class TransactionError extends APIError {
     constructor(cause: Error | string = null) {
-        super(500, 8000, 'transcation error', cause)
+        super(500, 8000, 'transaction error', cause)
         Object.setPrototypeOf(this, TransactionError.prototype)
         Error.captureStackTrace(this, TransactionError)
     }
