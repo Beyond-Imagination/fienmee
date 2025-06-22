6a857c4f26a59b41bee27c004eb1cec7ff97ae47
laggu91 <laggu91@gmail.com>
2025-06-22 09:45:01 +0900

    feat: show interest category list

--- apps/web/src/app/(navigation)/(home)/page.tsx
+++ apps/web/src/app/(navigation)/(home)/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { useSuspenseQuery } from '@tanstack/react-query'
 
 import SearchBar from '@/components/searchBar'
 import PreviewEventLst from '@/components/events/previewEventList'
 
-import { getEventsCategories } from '@/api/event'
 import { CategoryCode } from '@fienmee/types'
 
 export default function Home() {
@@ -24,23 +22,15 @@ export default function Home() {
 function PageComponent() {
     const today = new Date()
 
-    // TODO: remove get categories
-    const { data } = useSuspenseQuery({
-        queryKey: ['categories'],
-        queryFn: () => getEventsCategories(),
-        refetchOnWindowFocus: false,
-    })
-
     return (
         <div className="flex flex-col justify-items-center min-h-[80vh] my-6">
             <div className="px-8 mb-8">
                 <SearchBar />
             </div>
             <div className="grid gap-10 w-full px-1">
-                {/*TODO: change event category*/}
                 <PreviewEventLst title={`${today.getDate()}일 일정`} category={'dates'} />
                 <div className="bg-[#F6F5F5] shadow-inner shadow-[#E4E4E4] w-full h-2" />
-                <PreviewEventLst title="관심 카테고리" category={data.categories[0]._id} />
+                <PreviewEventLst title="관심 카테고리" category={'interest'} />
                 <div className="bg-[#F6F5F5] shadow-inner shadow-[#E4E4E4] w-full h-2" />
                 <PreviewEventLst title="인기 행사" category={CategoryCode.HOTEVENT.toString()} />
             </div>
