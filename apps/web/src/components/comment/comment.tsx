126ea2fa460e8b6725a5ef39de7b091d819fff10
sezeme <stellaa223@gmail.com>
2025-07-07 02:13:04 +0900

    feat: 로딩중 표시 생성

--- apps/web/src/components/comment/comment.tsx
+++ apps/web/src/components/comment/comment.tsx
@@ -7,6 +7,7 @@ import { deleteEventCommentById } from '@/api/event'
 import CommentOption from '@/components/comment/commentOption'
 import CommentUpdateField from '@/components/comment/commentUpdateField'
 import { UnlikeIcon } from '@/components/icon'
+import LoadingOverlay from '../loadingOverlay'
 
 interface Props {
     comment: IComment
@@ -36,23 +37,26 @@ export function EventComment({ comment }: Props) {
     }
 
     return (
-        <div className="flex flex-col gap-6 py-2 relative">
-            <div className="flex justify-between items-center">
-                <div>
-                    <div className="text-sm text-gray-500">{comment.nickname}</div>
-                    {isEdit ? (
-                        <CommentUpdateField comment={comment} onSuccess={() => setIsEdit(false)} />
-                    ) : (
-                        <div className="text-base text-gray-900">{comment.comment}</div>
-                    )}
-                </div>
-                <div className="flex flex-row justify-center items-center gap-2">
-                    <UnlikeIcon width="1.5rem" height="1.25rem" />
-                    {comment.isAuthor && (
-                        <CommentOption onEdit={() => setIsEdit(true)} onDelete={handleDelete} isDeleteDisabled={deleteMutation.isPending} />
-                    )}
+        <>
+            {deleteMutation.isPending && <LoadingOverlay />}
+            <div className="flex flex-col gap-6 py-2 relative">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <div className="text-sm text-gray-500">{comment.nickname}</div>
+                        {isEdit ? (
+                            <CommentUpdateField comment={comment} onSuccess={() => setIsEdit(false)} />
+                        ) : (
+                            <div className="text-base text-gray-900">{comment.comment}</div>
+                        )}
+                    </div>
+                    <div className="flex flex-row justify-center items-center gap-2">
+                        <UnlikeIcon width="1.5rem" height="1.25rem" />
+                        {comment.isAuthor && (
+                            <CommentOption onEdit={() => setIsEdit(true)} onDelete={handleDelete} isDeleteDisabled={deleteMutation.isPending} />
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
