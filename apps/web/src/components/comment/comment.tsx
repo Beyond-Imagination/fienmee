56a2b4c5b302201a21efddb0f8f25871cf1611d6
sezeme <stellaa223@gmail.com>
2025-07-13 19:26:08 +0900

    refactor: remove unnecessary Fragment

--- apps/web/src/components/comment/comment.tsx
+++ apps/web/src/components/comment/comment.tsx
@@ -37,26 +37,24 @@ export function EventComment({ comment }: Props) {
     }
 
     return (
-        <>
-            <div className="flex flex-col gap-6 py-2 relative">
-                {deleteMutation.isPending && <LoadingOverlay />}
-                <div className="flex justify-between items-center">
-                    <div>
-                        <div className="text-sm text-gray-500">{comment.nickname}</div>
-                        {isEdit ? (
-                            <CommentUpdateField comment={comment} onSuccess={() => setIsEdit(false)} />
-                        ) : (
-                            <div className="text-base text-gray-900">{comment.comment}</div>
-                        )}
-                    </div>
-                    <div className="flex flex-row justify-center items-center gap-2">
-                        <UnlikeIcon width="1.5rem" height="1.25rem" />
-                        {comment.isAuthor && (
-                            <CommentOption onEdit={() => setIsEdit(true)} onDelete={handleDelete} isDeleteDisabled={deleteMutation.isPending} />
-                        )}
-                    </div>
+        <div className="flex flex-col gap-6 py-2 relative">
+            {deleteMutation.isPending && <LoadingOverlay />}
+            <div className="flex justify-between items-center">
+                <div>
+                    <div className="text-sm text-gray-500">{comment.nickname}</div>
+                    {isEdit ? (
+                        <CommentUpdateField comment={comment} onSuccess={() => setIsEdit(false)} />
+                    ) : (
+                        <div className="text-base text-gray-900">{comment.comment}</div>
+                    )}
+                </div>
+                <div className="flex flex-row justify-center items-center gap-2">
+                    <UnlikeIcon width="1.5rem" height="1.25rem" />
+                    {comment.isAuthor && (
+                        <CommentOption onEdit={() => setIsEdit(true)} onDelete={handleDelete} isDeleteDisabled={deleteMutation.isPending} />
+                    )}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
