ce245f500b811528f57e64e180d9b5296616a506
Heesoo Jun <acesk123@gmail.com>
2025-08-15 21:29:22 +0900

    loading change

--- apps/web/src/app/(navigation)/search/loading.tsx
+++ apps/web/src/app/(navigation)/search/loading.tsx
@@ -4,8 +4,12 @@ import { ClipLoader } from 'react-spinners'
 
 export default function Loading() {
     return (
-        <div className="flex h-screen items-center justify-center bg-white">
-            <ClipLoader color="#FF6B6B" size={50} />
+        <div className="mx-auto w-full max-w-[420px] min-h-screen bg-white overflow-x-hidden">
+            <div className="flex h-[60vh] items-center justify-center">
+                {' '}
+                <ClipLoader color="#FF6B6B" size={50} />
+            </div>
+            <div className="h-16" />
         </div>
     )
 }
